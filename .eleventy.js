48824292a338a52e8fb852e37ec9fd0a84c28df7
gorushkin <gorushkin@gmail.com>
2022-08-13 22:10:02 +0300

    fix config

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('./admin/');
 
   // Watch the `css` directory for changes
   eleventyConfig.addWatchTarget('./src/css');
