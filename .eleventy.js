5c4be53ab6f51d03dfbabdcba70db2697401d5ef
gorushkin <gorushkin@gmail.com>
2022-08-13 22:02:24 +0300

    add cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy('./src/assets');
 
   // Watch the `css` directory for changes
-  eleventyConfig.addWatchTarget('src/css');
+  eleventyConfig.addWatchTarget('./src/css');
 
   eleventyConfig.addFilter('readableDate', (dateObj) => {
     return dateObj;
